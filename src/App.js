94c76e0918da3a031b9733ee2d28f28f08925a87
Hemang Rathod <hemangrathod01@gmail.com>
2023-02-09 07:09:22 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
+import React, { useEffect, useState } from 'react';
+import Table from "./Table";
+import States from './States';
 function App() {
+
+  const [data, setData] = useState([])
+  const [city, setCity] = useState([])
+  const [countryId, setCountryId] = useState()
+
+  const cityHandler = (value) => {
+    console.log(value)
+    setCountryId(value)
+  }
+
+  useEffect(() => {
+    fetch("https://interview-api.kodecreators.com/api/countries?page=1&per_page=10")
+      .then((response) => response.json())
+      .then((actualData) => setData(actualData.data));
+  }, []);
+
+  useEffect(() => {
+    fetch(`https://interview-api.kodecreators.com/api/states/?country_id=${countryId}&per_page=10`)
+      .then((response) => response.json())
+      .then((cityData) => setCity(cityData.data));
+  }, []);
+
+  console.log(data)
+  console.log(city)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Table data={data} rowsPerPage={5} onCountryId={cityHandler} />
+      <States data={city} rowsPerPage={5} />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Table.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import useTable from './hooks/useTable';
+import CountryFooter from './CountryFooter';
+
+function Table({ data, rowsPerPage, onCountryId }) {
+
+    const [page, setPage] = useState(1);
+    const { slice, range } = useTable(data, page, rowsPerPage);
+
+    const stateHandler = (event) => {
+        console.log(event.target.value)
+        onCountryId(event.target.value);
+    }
+
+
+    return (
+        <div className="w-full h-screen bg-black text-white">
+            <div className="mx-64 py-56">
+                <h1 className="text-4xl mb-10">Countries</h1>
+                <table className="grid place-items-center" id="country">
+                    {/* <div className="w-full border-2 border-black"> */}
+
+                    <tr className="grid grid-cols-2  w-full text-center rounded-t-xl">
+                        <td className="text-3xl ">Name</td>
+                        <td className="text-3xl ">Action</td>
+                    </tr>
+
+                    {/* </div> */}
+                    {/* <div className="w-full"> */}
+                    {slice.map((item) => {
+                        return (
+                            <tr className="grid grid-cols-2 w-full text-center">
+                                <td className="text-xl">{item.name}</td>
+                                <td className="text-xl"><button onClick={stateHandler} value={item.id} className="bg-blue-300 text-md rounded-lg w-24 m-1" type="">States</button></td>
+                            </tr>
+                        )
+                    })}
+                    {/* </div> */}
+                </table>
+                <CountryFooter
+                    range={range}
+                    slice={slice}
+                    setPage={setPage}
+                    page={page}
+                />
+            </div>
+        </div>
+    );
+}
+
+export default Table
