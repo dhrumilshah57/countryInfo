283b7826482a47fedab746ee82602e55f29b6183
dhrumilshah57 <dhrumilshah57>
2023-02-20 15:54:18 +0530

    final

--- src/Home.js
+++ src/Home.js
@@ -8,9 +8,9 @@ function Home() {
             <NavBar />
             <div className=''>
                 <div className='flex gap-56'>
-                    <Link to="/countries"><button className='border-2 w-56 h-24 '>Click Here For Countries</button></Link>
-                    <Link to="/rivers"><button className='border-2 w-56 h-24 '>Click Here For Rivers</button></Link>
-                    <Link to="/mountains"><button className='border-2 w-56 h-24 '>Click Here For Mountains</button></Link>
+                    <Link to="/countries"><button className='border-2 w-56 h-24 hover:bg-blue-300 transition ease-in-out hover:-translate-y-1 hover:text-black'>Click Here For Countries</button></Link>
+                    <Link to="/rivers"><button className='border-2 w-56 h-24 hover:bg-blue-300 transition ease-in-out hover:-translate-y-1 hover:text-black'>Click Here For Rivers</button></Link>
+                    <Link to="/mountains"><button className='border-2 w-56 h-24 hover:bg-blue-300 transition ease-in-out hover:-translate-y-1 hover:text-black'>Click Here For Mountains</button></Link>
                 </div>
             </div>
         </div>
--- src/PageNotFound.js
+++ src/PageNotFound.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const PageNotFound = () => {
     return (
-        <div className='bg-black h-screen justify-center flex flex-col items-center text-red-500'>
+        <div className='bg-black h-screen justify-center flex flex-col items-center text-red-500 transition'>
             <h1>404 Error</h1>
             <h1>Page Not Found</h1>
         </div>
